5b9359e84b69997a5018648f89b282d1ccc938b3
visavi <visavi.net@mail.ru>
2013-07-20 18:19:35 +0400

    Поправлена статистика форума

--- pages/statistik.php
+++ pages/statistik.php
@@ -8,22 +8,22 @@
 #                  ICQ  :  36-44-66                   #
 #  Вы не имеете право вносить изменения в код скрипта #
 #        для его дальнейшего распространения          #
-#-----------------------------------------------------#	
+#-----------------------------------------------------#
 require_once "../includes/start.php";
 require_once "../includes/functions.php";
 require_once "../includes/header.php";
 include_once "../themes/".$config['themes']."/index.php";
 
-echo '<img src="../images/img/profiles.gif" alt="image" /> <b>Статистика сайта</b><br /><br />'; 
+echo '<img src="../images/img/profiles.gif" alt="image" /> <b>Статистика сайта</b><br /><br />';
 
-$localfile = file_get_contents(DATADIR."local.dat"); 
+$localfile = file_get_contents(DATADIR."local.dat");
 $arrloc = explode("|",$localfile);
 
-$fileforum = file_get_contents(DATADIR."forum.dat"); 
-$dataforum = explode(":||:",$fileforum);
+$statforum = stats_forum();
+$dataforum = explode('/', $statforum);
 
-echo 'Тем в форуме: <b>'.(int)$dataforum[2].'</b><br />';
-echo 'Записей в форуме: <b>'.(int)$dataforum[3].'</b><br />';
+echo 'Тем в форуме: <b>'.(int)$dataforum[0].'</b><br />';
+echo 'Записей в форуме: <b>'.(int)$dataforum[0].'</b><br />';
 echo 'Записей в гостевой: <b>'.(int)$arrloc[0].'</b><br />';
 echo 'Всего новостей: <b>'.stats_allnews().'</b><br />';
 echo 'Комментариев в новостях: <b>'.(int)$arrloc[3].'</b><br />';
@@ -34,11 +34,9 @@ echo 'Записей в чате: <b>'.(int)$arrloc[8].'</b><br />';
 
 echo 'Забаненых: <b>'.stats_banned().'</b><br />';
 echo 'Забаненых по IP: <b>'.stats_ipbanned().'</b><br />';
-echo 'Зарегистрированных: <b>'.stats_users().'</b><br />'; 
-echo 'Логинов в черном списке: <b>'.stats_blacklogin().'</b><br />'; 
-echo 'E-mail в черном списке: <b>'.stats_blackmail().'</b><br />'; 
-
-$tot = array_sum($arrloc);
+echo 'Зарегистрированных: <b>'.stats_users().'</b><br />';
+echo 'Логинов в черном списке: <b>'.stats_blacklogin().'</b><br />';
+echo 'E-mail в черном списке: <b>'.stats_blackmail().'</b><br /><br />';
 
 //$arrloc[0] = Сообщений в гостевой
 //$arrloc[1] = Тем в форуме
@@ -49,8 +47,6 @@ $tot = array_sum($arrloc);
 //$arrloc[6] = Комментарий в библиотеке
 //$arrloc[7] = Комментарий в галерее
 //$arrloc[8] = Сообщений в чате
-
-echo '<br />Актив сайта: <b>'.(int)$tot.'</b> баллoв(а)<br /><br />';
 
 echo '<img src="../images/img/homepage.gif" alt="image" /> <a href="../index.php?'.SID.'">На главную</a>';
 
